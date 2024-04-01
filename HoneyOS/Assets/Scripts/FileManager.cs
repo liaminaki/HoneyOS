078150b7ac05a465f904f8ce9970eb0da08b415e
Dave Noceda <dbnoceda@up.edu.ph>
2024-04-01 11:48:50 +0800

    Edit File Manager UI

--- HoneyOS/Assets/Scripts/FileManager.cs
+++ HoneyOS/Assets/Scripts/FileManager.cs
@@ -6,85 +6,85 @@ using TMPro;
 // Define the FileManager class that extends the App class
 public class FileManager : App
 {   
-    private Node home;
-    private Node currentDirectory;
+    // private Node home;
+    // private Node currentDirectory;
 
-    [SerializeField] private TMP_Text _currentDirectoryAddress;
+    // [SerializeField] private TMP_Text _currentDirectoryAddress;
 
 
-    public void Start(){
-        home = new Node (new FileDescriptor("Home", "home"));
-        currentDirectory = home;
+    // public void Start(){
+    //     home = new Node (new FileDescriptor("Home", "home"));
+    //     currentDirectory = home;
 
-        UpdateCurrentDirectoryText();
-    }
+    //     UpdateCurrentDirectoryText();
+    // }
 
-    public void UpdateCurrentDirectoryText(){
-        _currentDirectoryAddress.text = currentDirectory.FileDescriptor.Path;
-    }
+    // public void UpdateCurrentDirectoryText(){
+    //     _currentDirectoryAddress.text = currentDirectory.FileDescriptor.Path;
+    // }
 
 
 }
 
-public class FileDescriptor {
-    public string Path { get; private set; }
-    public string Contents { get; set; }
-    public string Name { get; private set; }
+// public class FileDescriptor {
+//     public string Path { get; private set; }
+//     public string Contents { get; set; }
+//     public string Name { get; private set; }
 
-    public FileDescriptor(string path, string name)
-    {
-        Path = path;
-        Contents = "";
-        Name = name;
-    }
+//     public FileDescriptor(string path, string name)
+//     {
+//         Path = path;
+//         Contents = "";
+//         Name = name;
+//     }
 
-    public void SetName(string name)
-    {
-        Name = name;
-    }
+//     public void SetName(string name)
+//     {
+//         Name = name;
+//     }
 
-    public void SetPath(string path)
-    {
-        Path = path;
-    }
+//     public void SetPath(string path)
+//     {
+//         Path = path;
+//     }
 
-    public string GetContent()
-    {
-        return Contents;
-    }
+//     public string GetContent()
+//     {
+//         return Contents;
+//     }
 
-    public void SetContents(string contents)
-    {
-        Contents = contents;
-    }
-}
+//     public void SetContents(string contents)
+//     {
+//         Contents = contents;
+//     }
+// }
 
-public class Node {
-    public FileDescriptor FileDescriptor { get; private set; }
-    public List<Node> Children { get; private set; }
-    public Node Parent { get; private set; }
+// public class Node {
+//     public FileDescriptor FileDescriptor { get; private set; }
+//     public List<Node> Children { get; private set; }
+//     public Node Parent { get; private set; }
 
-    public Node(FileDescriptor fileDescriptor)
-    {
-        FileDescriptor = fileDescriptor;
-        Children = new List<Node>();
-    }
+//     public Node(FileDescriptor fileDescriptor)
+//     {
+//         FileDescriptor = fileDescriptor;
+//         Children = new List<Node>();
+//     }
 
-    public void SetParent(Node parent)
-    {
-        Parent = parent;
-    }
+//     public void SetParent(Node parent)
+//     {
+//         Parent = parent;
+//     }
 
-    public void AddChild(Node child)
-    {
-        child.SetParent(this);
-        Children.Add(child);
-    }
+//     public void AddChild(Node child)
+//     {
+//         child.SetParent(this);
+//         Children.Add(child);
+//     }
 
-    public void RemoveChild(Node child)
-    {
-        Children.Remove(child);
-    }
+//     public void RemoveChild(Node child)
+//     {
+//         Children.Remove(child);
+//     }
 
     
-}
+// }
