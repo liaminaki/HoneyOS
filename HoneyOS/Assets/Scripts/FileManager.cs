d1304343be5c71cf7a35f36308503cfc6b40e210
Dave Noceda <dbnoceda@up.edu.ph>
2024-04-09 12:20:14 +0800

    Create a separate CScript for FileSystem

--- HoneyOS/Assets/Scripts/FileManager.cs
+++ HoneyOS/Assets/Scripts/FileManager.cs
@@ -8,68 +8,68 @@ using Unity.VisualScripting;
 public class FileManager : App
 {   
 
-    [SerializeField] private TMP_Text _currentAddress;
-    private Node root;
-    private Node currentDirectory;
+    // [SerializeField] private TMP_Text _currentAddress;
+    // private Node root;
+    // private Node currentDirectory;
 
-    public FileManager (){
-        this.root = new Node (new FileDescriptor (">Home", "Home"));
-        currentDirectory = root;
-    }
+    // public FileManager (){
+    //     this.root = new Node (new FileDescriptor (">Home", "Home"));
+    //     currentDirectory = root;
+    // }
 
-    public void Start (){
-        UpdateAddress();
-    }
+    // // public void Start (){
+    // //     UpdateAddress();
+    // // }
 
-    public void UpdateAddress(){
-        if (currentDirectory != null && currentDirectory.FileDescriptor != null){
-            string path = currentDirectory.FileDescriptor.Path;
-            _currentAddress.text = path;
-        }
-        else{
-            Debug.Log ("Current directory of its file descriptor is null.");
-        }
-    }
+    // public void UpdateAddress(){
+    //     if (currentDirectory != null && currentDirectory.FileDescriptor != null){
+    //         string path = currentDirectory.FileDescriptor.Path;
+    //         _currentAddress.text = path;
+    //     }
+    //     else{
+    //         Debug.Log ("Current directory of its file descriptor is null.");
+    //     }
+    // }
 }
 
-public class FileDescriptor
-{
-    public string Path { get; set; }
-    public string Contents { get; set; }
-    public string Name { get; set; }
+// public class FileDescriptor
+// {
+//     public string Path { get; set; }
+//     public string Contents { get; set; }
+//     public string Name { get; set; }
 
-    public FileDescriptor(string path, string name)
-    {
-        Path = path;
-        Contents = "";
-        Name = name;
-    }
-}
+//     public FileDescriptor(string path, string name)
+//     {
+//         Path = path;
+//         Contents = "";
+//         Name = name;
+//     }
+// }
 
-public class Node
-{
-    public FileDescriptor FileDescriptor { get; }
-    public List<Node> Children { get; }
-    public Node Parent { get; private set; }
+// public class Node
+// {
+//     public FileDescriptor FileDescriptor { get; }
+//     public List<Node> Children { get; }
+//     public Node Parent { get; private set; }
 
-    public Node(FileDescriptor fileDescriptor)
-    {
-        FileDescriptor = fileDescriptor;
-        Children = new List<Node>();
-    }
+//     public Node(FileDescriptor fileDescriptor)
+//     {
+//         FileDescriptor = fileDescriptor;
+//         Children = new List<Node>();
+//     }
 
-    public void SetParent(Node parent)
-    {
-        Parent = parent;
-    }
+//     public void SetParent(Node parent)
+//     {
+//         Parent = parent;
+//     }
 
-    public void AddChild(Node child)
-    {
-        child.SetParent(this);
-        Children.Add(child);
-    }
+//     public void AddChild(Node child)
+//     {
+//         child.SetParent(this);
+//         Children.Add(child);
+//     }
 
-    public void RemoveChild(Node child){
-        Children.Remove(child);
-    }
-}
+//     public void RemoveChild(Node child){
+//         Children.Remove(child);
+//     }
+// }
---
+++ HoneyOS/Assets/Scripts/FileManagerSystem/FileSystem.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+// Define the FileManager class that extends the App class
+public class FileSystem : MonoBehaviour
+{   
+
+    [SerializeField] private TMP_Text _currentAddress;
+    private Node root;
+    private Node currentDirectory;
+
+    public FileSystem (){
+        this.root = new Node (new FileDescriptor (">Home", "Home"));
+        currentDirectory = root;
+    }
+
+    public void Start (){
+        UpdateAddress();
+    }
+
+    public void UpdateAddress(){
+        if (currentDirectory != null && currentDirectory.FileDescriptor != null){
+            string path = currentDirectory.FileDescriptor.Path;
+            _currentAddress.text = path;
+        }
+        else{
+            Debug.Log ("Current directory of its file descriptor is null.");
+        }
+    }
+}
+
+public class FileDescriptor
+{
+    public string Path { get; set; }
+    public string Contents { get; set; }
+    public string Name { get; set; }
+
+    public FileDescriptor(string path, string name)
+    {
+        Path = path;
+        Contents = "";
+        Name = name;
+    }
+}
+
+public class Node
+{
+    public FileDescriptor FileDescriptor { get; }
+    public List<Node> Children { get; }
+    public Node Parent { get; private set; }
+
+    public Node(FileDescriptor fileDescriptor)
+    {
+        FileDescriptor = fileDescriptor;
+        Children = new List<Node>();
+    }
+
+    public void SetParent(Node parent)
+    {
+        Parent = parent;
+    }
+
+    public void AddChild(Node child)
+    {
+        child.SetParent(this);
+        Children.Add(child);
+    }
+
+    public void RemoveChild(Node child){
+        Children.Remove(child);
+    }
+}
