439e3d6e0ce9e22572fcc75583f1b073ff20bd24
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2024-04-15 02:59:14 +0800

    Integrate Help app in desktop

--- HoneyOS/Assets/Scripts/Help.cs
+++ HoneyOS/Assets/Scripts/Help.cs
@@ -6,20 +6,20 @@ using TMPro;
 // Define the FileManager class that extends the App class
 public class Help : App
 {   
-    public TextMeshProUGUI textMeshPro;
+    // public TextMeshProUGUI textMeshPro;
 
-    // Override the ResetApp method to reset FileManager to default state
-    protected override void Reset()
-    {   
-        textMeshPro.text = "I AM IN HELPPPP.";
-        // Implement FileManager-specific reset behavior here
-    }
+    // // Override the ResetApp method to reset FileManager to default state
+    // protected override void Reset()
+    // {   
+    //     textMeshPro.text = "I AM IN HELPPPP.";
+    //     // Implement FileManager-specific reset behavior here
+    // }
    
-    public void ChangeText()
-    {
-        // Assign the new text to the TextMeshPro component
-        textMeshPro.text = "I AM IN HELPPPP. I MODIFIED SHITZ.";
-    }
+    // public void ChangeText()
+    // {
+    //     // Assign the new text to the TextMeshPro component
+    //     textMeshPro.text = "I AM IN HELPPPP. I MODIFIED SHITZ.";
+    // }
 
     // Add FileManager-specific functionality here if needed
 
