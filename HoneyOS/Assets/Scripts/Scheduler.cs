2cbd1f6450a5b34f1cbc4fe8f0d236ac7a0a5a85
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2024-05-06 21:42:52 +0800

    Init scheduler app

---
+++ HoneyOS/Assets/Scripts/Scheduler.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+// Define the FileManager class that extends the App class
+public class Scheduler: App
+{   
+    // public TextMeshProUGUI textMeshPro;
+
+    // // Override the ResetApp method to reset FileManager to default state
+    // protected override void Reset()
+    // {   
+    //     textMeshPro.text = "I AM IN HELPPPP.";
+    //     // Implement FileManager-specific reset behavior here
+    // }
+   
+    // public void ChangeText()
+    // {
+    //     // Assign the new text to the TextMeshPro component
+    //     textMeshPro.text = "I AM IN HELPPPP. I MODIFIED SHITZ.";
+    // }
+
+    // Add FileManager-specific functionality here if needed
+
+    
+}
