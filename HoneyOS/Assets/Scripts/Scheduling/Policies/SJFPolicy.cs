314e1d3454c3a7516ecfcc83e4ee41d9e83ad4ce
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2024-05-06 22:45:48 +0800

    Add SJF functionality

--- HoneyOS/Assets/Scripts/Scheduling/Policies/SJFPolicy.cs
+++ HoneyOS/Assets/Scripts/Scheduling/Policies/SJFPolicy.cs
@@ -7,14 +7,31 @@ using System.Collections.Generic;
 public class SJFPolicy : Policy
 {
     public override Process GetRunningProcess(List<Process> processList)
-    {
+    {   
+        // No process to run
         if (processList.Count == 0)
-            return null; // No process to run
+            return null; 
 
-        else
-        {
-            
+        // Initialize the first process as the shortest job
+        Process shortestJob = processList[0];
+        
+        foreach (Process process in processList)
+        {   
+            // Check if the process is shorter than current shortest.
+            if (process.BurstTime < shortestJob.BurstTime)
+            {
+                shortestJob = process;
+            }
+
+            /*
+                Logic already handles FCFS if there are more than one of the same shortest burst time.
+                For instance, if current shortest has 5 burst time and the next process has also 5 burst time,
+                the shortest job will not be changed since 5 !< 5.
+                Since the list is ordered from the arrival of process, it makes sense that 
+                for every same shortest time, the current (i.e. the one who first came) is the one executed.
+            */
         }
-        return 
+        
+        return shortestJob;
     }
 }
